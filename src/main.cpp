9d96e6bae6b1247d55e366cd3cdb7b30331c49d0
Patrick Shriwise <pshriwise@gmail.com>
2023-01-02 16:53:20 -0600

    Some minor updates to create a context and module

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,21 @@
 
+#include <gprt.h>
+
+// include for GPRT structs
+#include "sharedCode.h"
+
+extern GPRTProgram dev_code;
+
 int main() {
+
+    // create a ray tracing context
+    GPRTContext gprt = gprtContextCreate(nullptr, 1);
+
+
+    GPRTModule module = gprtModuleCreate(gprt, dev_code);
+
+
     return 0;
+
+
 }
