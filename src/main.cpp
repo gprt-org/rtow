f5d144cf164b6767e638649a4559fd688439c4b3
Patrick Shriwise <pshriwise@gmail.com>
2023-01-02 22:45:45 -0600

    Hello world image

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,39 @@
 
 extern GPRTProgram dev_code;
 
+
+const int image_width = 256;
+const int image_height = 256;
+
 int main() {
 
-    // create a ray tracing context
-    GPRTContext gprt = gprtContextCreate(nullptr, 1);
+  // create a ray tracing context
+  GPRTContext gprt = gprtContextCreate(nullptr, 1);
 
+  GPRTModule module = gprtModuleCreate(gprt, dev_code);
 
-    GPRTModule module = gprtModuleCreate(gprt, dev_code);
+  GPRTRayGenOf<RayGenData> rayGen =
+    gprtRayGenCreate<RayGenData>(gprt, module, "simpleRayGen");
 
+  gprtBuildPipeline(gprt);
 
-    return 0;
+  GPRTBufferOf<uint32_t> frameBuffer =
+    gprtDeviceBufferCreate<uint32_t>(gprt, image_width * image_height);
 
+  RayGenData *data = gprtRayGenGetPointer(rayGen);
+  data->fbPtr = gprtBufferGetHandle(frameBuffer);
+  data->fbSize = int2(image_width, image_height);
 
+  gprtBuildShaderBindingTable(gprt, GPRT_SBT_RAYGEN);
+
+  gprtRayGenLaunch2D(gprt, rayGen, image_width, image_height);
+
+  gprtBufferSaveImage(frameBuffer, image_width, image_height, "test.png");
+
+  gprtBufferDestroy(frameBuffer);
+  gprtRayGenDestroy(rayGen);
+  gprtModuleDestroy(module);
+  gprtContextDestroy(gprt);
+
+  return 0;
 }
--- src/sharedCode.h
+++ src/sharedCode.h
@@ -9,8 +9,8 @@
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -22,29 +22,9 @@
 
 #include "gprt.h"
 
-/* variables for the triangle mesh geometry */
-struct DPTriangleData
-{
-  /*! array/buffer of vertex indices */
-  alignas(16) gprt::Buffer index; // vec3f*
-  /*! array/buffer of vertex positions */
-  alignas(16) gprt::Buffer vertex; // float *
-  /*! array/buffer of AABBs */
-  alignas(16) gprt::Buffer aabbs;
-  /*! array/buffer of double precision rays */
-  alignas(16) gprt::Buffer dpRays;
-};
-
-struct RayGenData
-{
-  alignas(4)  int frameId;
-  alignas(16) gprt::Buffer dpRays;
-  alignas(16) gprt::Accel world;
-  alignas(16) gprt::Buffer distances;
-};
-
-/* variables for the miss program */
-struct MissProgData
-{
-  alignas(4) int temp;
+// note! HLSL aligns to float4 boundaries!
+struct RayGenData {
+  // pointers are represented using uint64_t
+  alignas(16) gprt::Buffer fbPtr;
+  alignas(8) int2 fbSize;
 };
